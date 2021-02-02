56d4f132ea351a55a6d971fe5307b54b7ee37559
hq <1422127065@qq.com>
2021-02-02 10:54:45 +0800

    add:复杂 简单的邮件发送

--- asyntask/src/test/java/com/hq/AsyntaskApplicationTests.java
+++ asyntask/src/test/java/com/hq/AsyntaskApplicationTests.java
@@ -6,6 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @SpringBootTest
 class AsyntaskApplicationTests {
@@ -24,5 +29,24 @@ class AsyntaskApplicationTests {
 
         mailSender.send(simpleMailMessage);
     }
+
+    @Test
+    void contextLoads2() throws MessagingException {
+        //一个简单的邮件发送
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        //组装
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        //正文
+        helper.setSubject("哈哈哈哈 测试邮件");
+        helper.setText("测试内容");
+        //附件
+        helper.addAttachment("1.jpg",new File("C:\\Users\\hq\\Desktop\\1.jpg"));
+        helper.setTo("1418856457@qq.com");
+        helper.setFrom("1422127065@qq.com");
+
+        mailSender.send(mimeMessage);
+    }
+    
+    
 
 }
